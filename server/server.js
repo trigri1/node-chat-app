f3f9bd63df1407a5b931aeda2b251dcdf83678f6
gohar-ali <gohar.ali@starzplayarabia.com>
2019-01-02 22:20:30 +0500

    socket io connection logic created

--- server/server.js
+++ server/server.js
@@ -1,16 +1,24 @@
 const path = require('path');
-const Express = require('express');
+const express = require('express');
+const app = new express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 const port = process.env.PORT || 3000
-var app = new Express();
 
 const publicPath = path.join(__dirname, '../public');
-app.use(Express.static(publicPath))
+app.use(express.static(publicPath))
 
+io.on('connection', (socket) => {
+    console.log('new user connected');
 
+    socket.on('disconnect',()=>{
+        console.log('Disconnected from client');
+    });
+});
 
 if (!module.parent) {
-    app.listen(port, () => {
+    server.listen(port, () => {
         console.log(`listening at port ${port}`);
     });
 };
